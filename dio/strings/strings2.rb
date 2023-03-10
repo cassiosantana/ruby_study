6e3185dff13e8b0cb6746e4ea6bf90b27084804e
Cassio Santana <cassioo.roger@gmail.com>
2023-03-10 19:45:02 -0300

    Algumas operações com string

---
+++ dio/strings/strings2.rb
@@ -0,0 +1,13 @@
+# operações com string
+puts "Cassio"[0]
+puts "Cassio"[-1]
+puts "Cassio".chars
+puts "Cassio"[0,3]
+puts "Cassio" * 3
+puts "              Cassio Roger Santana                   ".strip
+puts "cassio".upcase
+puts "CASSIO".downcase
+puts "cassio".capitalize
+puts "cassio roger de santana".split
+puts "cassio roger de-santana".split
+puts "cassio-roger-de_santana".split("-")
