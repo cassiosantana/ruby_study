e812f4f4035ce745c57053c6718de5378e105b32
Cassio Santana <cassioo.roger@gmail.com>
2023-03-13 07:50:08 -0300

    Laços - mais exemplos

--- dio/lacos/lacos1.rb
+++ dio/lacos/lacos1.rb
@@ -1,8 +1,19 @@
 # while
 valor = 5
-while valor > 5
+while valor > 0
     puts "Valor é: %05d" % valor
     valor -= 1
+end
+
+puts "#" * 30
+
+valor2 = 5
+while valor2 > 0
+    puts "Valor é: #{valor2} ; Laço com break"
+
+    break if valor2 == 3
+    
+    valor2 -= 1
 end
 
 puts "#" * 30
@@ -44,9 +55,22 @@ end
 
 puts "#" * 30
 
-hash = {nome: "Cassio", idade: 33}
+(0..5).each do |numero|
+    puts "Número do range: #{numero}"
+end
+
+puts "#" * 30
+
+hash = {nome: "Cassio", idade: 33} #############################################3333
 hash.each do |chave, valor|
     puts "Minha chave é #{chave} com o valor é #{valor}"
+end
+
+puts "#" * 30
+
+votos = {cassio: 1_333_444}
+votos.each do |nome, quantidade_de_votos|
+    puts "#{nome}, #{quantidade_de_votos}"
 end
 
 puts "#" * 30
@@ -58,7 +82,7 @@ until valor == 10
    valor += 1
 end 
 
-# comandos em laços
+# COMANDOS EM LAÇOS
 # break - sai do laço
 # return - sai do método além de sair do laço
 # next - próxima iteração
