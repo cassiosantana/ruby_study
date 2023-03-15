0a2ad124a2f3c89d6463ef8f571724c5644e945c
Cassio Santana <cassioo.roger@gmail.com>
2023-03-15 10:46:54 -0300

    Execicio OO - contas bancárias

---
+++ ruby_oo/conta_bancaria/entities/account.rb
@@ -0,0 +1,22 @@
+class Account
+    attr_reader :number
+    attr_accessor :holder, :balance
+    def initialize(number, holder, amount=0.0)
+        @number = number
+        @holder = holder
+        @balance = deposit(amount)
+    end
+
+    def deposit(amount)
+        balance =+ amount
+    end
+
+    def withdraw(amount)
+        raise "Insufficient account balance" if balance < amount
+        balance -= amount
+    end
+
+    def account_data
+        puts "Account number: #{number}, Holder: #{holder}, Balance: #{balance}"
+    end
+end
---
+++ ruby_oo/conta_bancaria/main.rb
@@ -0,0 +1,27 @@
+require "./entities/account"
+
+class Main 
+    def initialize
+        
+        puts "Enter account details"
+        print "Account number: "
+        number = gets.chomp.to_i
+        print "Account holder: "
+        holder = gets.chomp
+        print "Do you want to make an initial deposit? ( y | n ) "
+        response = gets.chomp
+
+        if response[0] == 'y' || 'Y'
+            print "What amount to deposit? "
+            amount = gets.chomp.to_i
+            acc1 = Account.new(number, holder, amount)
+        else
+            acc1 = Account.new(number, holder)
+        end
+
+        puts "-" * 100
+        acc1.account_data
+    end
+end
+
+Main.new
