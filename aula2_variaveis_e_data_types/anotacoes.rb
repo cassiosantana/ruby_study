8b7075ed815e874785ec9c752b2f4ecf0b2d222c
Cassio Santana <cassioo.roger@gmail.com>
2023-03-22 09:52:28 -0300

    feat(aula-3-variáveis-e-data-types): tipos de dados em ruby

---
+++ aula2_variaveis_e_data_types/anotacoes.rb
@@ -0,0 +1,20 @@
+# TIPOS DE DADOS EM RUBY
+# 
+# EM RUBY TUDO É OBJETO
+# e como são objetos existem métodos para serem usados.
+# a.to_i - método para converter para string
+# a.to_s - método para converter para symbol
+# 
+# nil
+# string
+# fixnum
+# integer
+# numeric
+# float
+# nilclass
+# hash
+# symbol
+# array
+# range
+
+#
