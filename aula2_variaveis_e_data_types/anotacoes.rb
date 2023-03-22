cc26ac7e1c157f9e50aea30b2666830c4865ad88
Cassio Santana <cassioo.roger@gmail.com>
2023-03-22 10:12:44 -0300

    feat(aula-3-variáveis-e-data-types): verificando o tipo de objetos

--- aula2_variaveis_e_data_types/anotacoes.rb
+++ aula2_variaveis_e_data_types/anotacoes.rb
@@ -1,3 +1,4 @@
+# https://www.geeksforgeeks.org/ruby-data-types/
 # TIPOS DE DADOS EM RUBY
 # 
 # EM RUBY TUDO É OBJETO
@@ -17,4 +18,19 @@
 # array
 # range
 
-#
+# TIPOS DE VARIÁVEIS EM RUBY
+
+# variáveis simples
+x = 1
+
+# variável de instãncia
+@x2 = 1
+
+# variável de classe que pode ser acessada por instancias e subclasses
+# @@x3 = 2
+
+# variável global | muito cuidado com este tipo de variável
+$x4 = 4
+
+# constantes | letras maiúsculas ou começando em maiúsculas e por isso uma classe é uma constante
+XLS = 5
---
+++ aula2_variaveis_e_data_types/aula2.rb
@@ -0,0 +1,6 @@
+# verificando o tipo
+puts 1.class
+puts 1.class.superclass
+puts 1.class.superclass.superclass
+puts 1.class.superclass.superclass.superclass
+p 1.class.superclass.superclass.superclass.superclass
