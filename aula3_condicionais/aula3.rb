10521a976806931e726d46583d529b5a7754f4ca
Cassio Santana <cassioo.roger@gmail.com>
2023-03-22 14:18:23 -0300

    feat(aula-4-condicionais): formas de utilizar o if

---
+++ aula3_condicionais/aula3.rb
@@ -0,0 +1,25 @@
+require 'byebug'
+
+debugger
+
+# if
+a = 1
+if a == 1
+    puts 'a == 1'
+end
+
+if a == 1 then puts 'a == 1' end
+
+if a == 2
+    puts 'a == 2'
+else
+    puts 'a não é igual a 2'
+end
+
+if a == 2
+    puts 'a == 2'
+elsif a == 3
+    puts 'a == 3'
+else
+    puts 'o a não é igual a 2 nem a 3'
+end
