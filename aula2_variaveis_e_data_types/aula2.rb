287c8dacda16add844f34b35699ec1f29abfcac4
Cassio Santana <cassioo.roger@gmail.com>
2023-03-22 10:24:52 -0300

    feat(aula-3-variáveis-e-data-types): verificando os tipos com o byebug

--- aula2_variaveis_e_data_types/aula2.rb
+++ aula2_variaveis_e_data_types/aula2.rb
@@ -1,6 +1,23 @@
+require 'byebug'
+
+debugger
+
 # verificando o tipo
 puts 1.class
 puts 1.class.superclass
 puts 1.class.superclass.superclass
 puts 1.class.superclass.superclass.superclass
 p 1.class.superclass.superclass.superclass.superclass
+
+h = {}
+puts "Stringy string McString!".class
+puts 1.class
+puts 1.class.superclass
+puts 1.class.superclass.superclass
+puts 4.3.class
+puts 4.3.class.superclass
+puts nil.class
+puts h.class
+puts :symbol.class
+puts [].class
+puts (1..8).class
