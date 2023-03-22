6e170beda7638d11685276947945aec31cfd69a2
Cassio Santana <cassioo.roger@gmail.com>
2023-03-22 10:37:32 -0300

    doc(aula-3-variáveis-e-data-types): adicionado observação quanto tipos do ruby serem fortes

--- aula2_variaveis_e_data_types/aula2.rb
+++ aula2_variaveis_e_data_types/aula2.rb
@@ -1,6 +1,6 @@
 require 'byebug'
 
-debugger
+# debugger
 
 # verificando o tipo
 puts 1.class
@@ -21,3 +21,8 @@ puts h.class
 puts :symbol.class
 puts [].class
 puts (1..8).class
+
+# obs
+# o ruby é uma linguagem dinâmica mas é fortemente tipado
+# 'a' + 1 não é permitido no ruby justamente por ser fortemente tipado
+puts 'a' + 1.to_s
