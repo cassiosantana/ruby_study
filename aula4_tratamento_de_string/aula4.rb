4bf1af4023509a7d418e5ccd47c2bfdca3007f9e
Cassio Santana <cassioo.roger@gmail.com>
2023-03-23 07:32:13 -0300

    feat(aula-5-tratamento-de-strings): substring

--- aula4_tratamento_de_string/aula4.rb
+++ aula4_tratamento_de_string/aula4.rb
@@ -12,7 +12,6 @@ b = 'teste 3'
 b.gsub!('3', '2')
 puts b
 
-
 # concatenação
 
 # esta forma só funciona com aspas duplas " "
@@ -24,3 +23,7 @@ puts "Cassio" + e
 
 puts "Cassio" << " Santana"
 puts c << e
+
+# substring
+texto = 'aula de hoje'
+puts texto[8, 10]
