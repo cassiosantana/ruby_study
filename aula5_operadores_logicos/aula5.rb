b8a69959619b683266df7d8aba7247af80842f18
Cassio Santana <cassioo.roger@gmail.com>
2023-03-23 09:04:50 -0300

    feat(aula-6-operadores-lógicos): onde utilizar?, exemplos

--- aula5_operadores_logicos/aula5.rb
+++ aula5_operadores_logicos/aula5.rb
@@ -1,1 +1,27 @@
+# operadores lógicos
+require 'byebug'
 
+# onde utilizar
+# em condicionais
+# ex. if, unless, while ...
+
+a = 1
+b = 2
+c = 4
+
+if 1==1 or 2==2
+    puts 'entrou no if'
+end
+
+unless b==1 && a==3
+    puts 'mostrou unless 1'
+end
+
+unless b==1 and a==3
+    puts 'mostrou unless 1'
+end
+
+while a <= b or b > c
+    puts 'olá'
+    a += 1
+end
