bd3e437bbc77dcaccc3255d16b1f60cd0a9522e7
Cassio Santana <cassioo.roger@gmail.com>
2023-03-24 09:20:33 -0300

    feat(aula-7-looping): while

---
+++ aula6_looping/aula6.rb
@@ -0,0 +1,18 @@
+# looping
+require 'byebug'
+
+# while
+top = 4
+now = 0
+while now < top
+    puts "while #{now} .. #{top}"
+    now += 1
+end
+
+puts "----------------------------------------"
+
+# while em uma linha
+top = 4
+now = 0
+# puts "while #{now += 1} .. #{top}" while now < top
+puts "while #{now += 1} .. #{top}" while now < top
