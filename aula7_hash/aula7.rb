34eac50d41c05e98a8a78aa1962a1c95dc9164b3
Cassio Santana <cassioo.roger@gmail.com>
2023-03-25 12:17:53 -0300

    feat(aula-8-hash): convertendo chave string em symbol

--- aula7_hash/aula7.rb
+++ aula7_hash/aula7.rb
@@ -30,3 +30,12 @@ a['nome'] = 'Paulo'
 a['telefone'] = 55555555
 puts a['nome']
 puts a['telefone']
+
+# convertendo chave string em symbol
+a['nome1'.to_sym] = 'Bruno'
+a['telefone1'.to_sym] = 33333333
+p a['nome1'] # chave não declarada
+p a['telefone1'] # chave não declarada
+puts a[:nome1]
+puts a[:telefone1]
+
