64f82c723d3698d5c01b87dbd2f2caf966630c86
Cassio Santana <cassioo.roger@gmail.com>
2023-03-25 12:43:08 -0300

    feat(aula-8-hash): como utilizar um hash em um array

--- aula7_hash/aula7.rb
+++ aula7_hash/aula7.rb
@@ -39,3 +39,28 @@ p a['telefone1'] # chave não declarada
 puts a[:nome1]
 puts a[:telefone1]
 
+# como utilizar um hash em um array
+loop do
+    puts "Bem vindo ao programa"
+    puts "Digite 0 para sair ou 1 para continuar"
+    valor = gets.chomp.to_i
+
+    break if valor == 0
+
+    alunos = []
+    3.times do
+        aluno = {}
+        puts "Digite o nome do aluno:"
+        aluno[:nome] = gets.chomp
+        puts "Digite o telefone do aluno"
+        aluno[:telefone] = gets.chomp
+
+        alunos << aluno
+    end
+
+    alunos.each do |aluno|
+        puts "=" * 50
+        puts "aluno: #{aluno[:nome]}, telefone: #{aluno[:telefone]}"
+    end
+    puts "=" * 50
+end
