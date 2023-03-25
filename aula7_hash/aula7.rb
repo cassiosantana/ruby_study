eae051d6ac5b19bfa98997d4b0893f38070eda2a
Cassio Santana <cassioo.roger@gmail.com>
2023-03-25 11:55:06 -0300

    feat(aula-8-hash): como declarar

---
+++ aula7_hash/aula7.rb
@@ -0,0 +1,20 @@
+# hash 
+
+# formas de declarar
+a = {}
+puts a.class
+
+a = {nome: 'cassio', telefone: 988888888}
+puts a
+
+a = {'nome' => 'roger', 'telefone' => 899999999}
+puts a
+
+a = {:nome => 'santana', :telefone => 77777777}
+puts a
+
+a = {
+    nome: 'cassio', 
+    telefone: 988888888
+}
+puts a
