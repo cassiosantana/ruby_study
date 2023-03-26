eb68d608006cfedd010af0a0bd5d07ff946ccbd0
Cassio Santana <cassioo.roger@gmail.com>
2023-03-26 12:43:35 -0300

    feat(aula-12-poo-accessors): attr_accessor

--- aula11_poo_accessors/aula11.rb
+++ aula11_poo_accessors/aula11.rb
@@ -1,1 +1,27 @@
 # accessors
+
+# gets e sets
+class Carro
+    # cria automáticamente os métodos get e set e a variável nome
+    attr_accessor :nome, :cor, :pneu, :porta, :painel
+end
+
+carro = Carro.new
+carro.nome = 'Gol'
+puts carro.nome
+
+
+# o attr_accessor executa um código semelhante a este:
+# def attr_accessor(*args)
+#     args.each do |propriedade|
+#         eval ("
+#             def #{propriedade}=(value)
+#                 @#{propriedade} = value
+#             end
+
+#             def #{propriedade}
+#                 @#{propriedade}
+#             end
+#         ")
+#     end
+# end
