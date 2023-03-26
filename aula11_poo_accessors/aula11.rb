04a36025687a616d13103f45f0a1aa876210d7d7
Cassio Santana <cassioo.roger@gmail.com>
2023-03-26 12:53:23 -0300

    feat(aula-12-poo-accessors): attr_writer

--- aula11_poo_accessors/aula11.rb
+++ aula11_poo_accessors/aula11.rb
@@ -3,14 +3,18 @@
 # gets e sets
 class Carro
     # cria automáticamente os métodos get e set e a variável nome
-    attr_accessor :cor, :pneu, :porta, :painel
+    attr_accessor :cor, :pneu, :painel
     # dá acesso apenas de leitura | método get
     attr_reader :nome
+    # permite mudar o valor de atributo | método set
+    attr_writer :porta
 end
 
 carro = Carro.new
 # carro.nome = 'Gol' | método set não foi definido
 puts carro.nome
+carro.porta = 4
+# puts carro.porta | método get não foi definido
 
 
 # o attr_accessor executa um código semelhante a este:
