4019e079b98db8736299913d81177111fe603c64
Cassio Santana <cassioo.roger@gmail.com>
2023-03-27 07:58:19 -0300

    feat(aula-17-metaprogramming-runtime): adicionada classe Carro

--- aula13_metaprograming_runtime/aula13.rb
+++ aula13_metaprograming_runtime/aula13.rb
@@ -17,3 +17,18 @@ end
 
 puts "------------------------------"
 puts String.mostrar_nova_string2
+
+puts "------------------------------"
+
+class Carro
+    def initialize(nome = "Modelo padrão")
+        @nome = nome
+    end
+
+    attr_accessor :nome, :porta, :painel, :roda
+    attr_reader :pneu
+    
+    def mostrar(marca = "Marca padrão")
+        puts "Marca: #{marca} - Modelo: #{self.nome}"
+    end
+end
