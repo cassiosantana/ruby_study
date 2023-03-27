e380b5c90c804104f69d8ff51ab3bd5c13a29b2d
Cassio Santana <cassioo.roger@gmail.com>
2023-03-27 07:35:33 -0300

    feat(aula-17-metaprogramming-runtime): definindo um método em tempo de execução para uma instância de string

--- aula13_metaprograming_runtime/aula13.rb
+++ aula13_metaprograming_runtime/aula13.rb
@@ -1,2 +1,11 @@
 # metaprograming - runtime
 
+a = "Danilo"
+
+# definindo um método em tempo de execução alterando a instância
+def a.mostrar_nova_string
+    "nova string"
+end
+
+puts a
+puts a.mostrar_nova_string
