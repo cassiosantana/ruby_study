d0ea830abf9a9d981ced35d5a81d36ebb08b336c
Cassio Santana <cassioo.roger@gmail.com>
2023-03-28 19:59:48 -0300

    feat(aula-14-poo-public-private-protected): definido um método privado

--- aula14_poo_public_private_protected/aula14.rb
+++ aula14_poo_public_private_protected/aula14.rb
@@ -1,1 +1,10 @@
 # poo_public_private_protected
+
+require_relative 'carro'
+require_relative 'fiesta'
+
+carro = Carro.new
+carro.mostrar
+
+# acessar método privado diretamente causa erro.
+# puts carro.algo_mais
--- aula14_poo_public_private_protected/carro.rb
+++ aula14_poo_public_private_protected/carro.rb
@@ -8,5 +8,13 @@ class Carro
     
     def mostrar(marca = "Marca padrão")
         puts "Marca: #{marca} - Modelo: #{self.nome}"
+        puts algo_mais
+    end
+
+    # private dá acesso apenas na própria classe
+    # para acessa-lo é necessário um método público interno da classe que o acesse
+    private
+    def algo_mais
+        "este é um método privado"
     end
 end
