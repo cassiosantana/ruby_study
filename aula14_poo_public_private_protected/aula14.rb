b0d679de0e27d3b857a75a8cfcc1e9264b0b1140
Cassio Santana <cassioo.roger@gmail.com>
2023-03-28 20:14:58 -0300

    feat(aula-14-poo-public-private-protected): testando um método privado na subclasse

--- aula14_poo_public_private_protected/aula14.rb
+++ aula14_poo_public_private_protected/aula14.rb
@@ -9,3 +9,12 @@ puts carro.algo_mais2
 
 # acessar método privado diretamente causa erro.
 # puts carro.algo_mais
+puts "------------------------------------------"
+
+fiesta = Fiesta.new
+# o ruby tem uma peculiariadade quando falamos de atributos privados
+# no ruby os atributos e métodos privados são exergados pelas subclasses
+# podemos entender que o ruby entende que o privado deve ser acessado por ser
+# uma instãncia da superclasse
+# mas se fosse definido Fiesta.new.mostrar_privado geraria o erro
+puts fiesta.mostrar_privado
--- aula14_poo_public_private_protected/fiesta.rb
+++ aula14_poo_public_private_protected/fiesta.rb
@@ -1,3 +1,8 @@
 class Fiesta < Carro
     attr_accessor :cor
+
+    def mostrar_privado
+        algo_mais
+        # Fiesta.new.algo_mais # causa erro por ser outra insância
+    end
 end
