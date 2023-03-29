3f054496c491a3a7920c911116572329823c3689
Cassio Santana <cassioo.roger@gmail.com>
2023-03-29 13:23:39 -0300

    feat(aula-16-interface-abstração-e-singleton): interface 'minha_interface' definida

---
+++ aula16_poo_interface_abstacao_singleton/minha_interface.rb
@@ -0,0 +1,13 @@
+class MinhaInterface
+  def initialize
+    raise "classe não pode ser instanciada nem herdada, apenas implementada"
+  end
+
+  def test_1
+    raise "método para ser implementado 1"
+  end
+
+  def test_2
+    raise "método para ser implementado 2"
+  end
+end
