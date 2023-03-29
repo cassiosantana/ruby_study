20493129aca255401b5c16384097ce8452b05522
Cassio Santana <cassioo.roger@gmail.com>
2023-03-28 21:06:33 -0300

    feat(aula-15-poo-polimorfismo-e-overload): sobrescrição de métodos

--- aula15_poo_polimorfismo_overload/aula15.rb
+++ aula15_poo_polimorfismo_overload/aula15.rb
@@ -2,3 +2,9 @@
 
 require_relative 'carro'
 require_relative 'golf'
+
+# polimorfismo é basicamente fazer subclasses se comportarem de formas diferentes
+# de acordo com as sobrescrições definidas
+Carro.new.mostrar
+puts "-------------------------"
+Golf.new.mostrar
--- aula15_poo_polimorfismo_overload/golf.rb
+++ aula15_poo_polimorfismo_overload/golf.rb
@@ -1,3 +1,6 @@
 class Golf < Carro
-    
+    # no Ruby sobrescrever um método é apenas redefini-lo na subclasse.
+    def mostrar
+        puts "este é o método mostrar da classe golf"
+    end
 end
