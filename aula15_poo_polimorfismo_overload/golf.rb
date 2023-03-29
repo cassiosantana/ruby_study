cf0cbd8ddcea745c9dfa9b92049646f6e99d98db
Cassio Santana <cassioo.roger@gmail.com>
2023-03-29 11:36:42 -0300

    feat(aula-15-poo-polimorfismo-e-overload): uso da palavra super em método

--- aula15_poo_polimorfismo_overload/golf.rb
+++ aula15_poo_polimorfismo_overload/golf.rb
@@ -1,6 +1,8 @@
 class Golf < Carro
     # no Ruby sobrescrever um método é apenas redefini-lo na subclasse.
     def mostrar
+        # uso da palavra super neste caso está chamando o método original
+        super
         puts "este é o método mostrar da classe golf"
     end
 end
