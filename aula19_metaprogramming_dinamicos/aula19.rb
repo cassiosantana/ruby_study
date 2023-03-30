e08654ce40247f800c5d1dfd93b06179725fcabc
Cassio Santana <cassioo.roger@gmail.com>
2023-03-30 09:42:34 -0300

    feat(aula-19-metaprogramming-dinâmicos): proc

--- aula19_metaprogramming_dinamicos/aula19.rb
+++ aula19_metaprogramming_dinamicos/aula19.rb
@@ -10,3 +10,32 @@ l = lambda do |p1, p2|
 end
 
 puts l.call 5, 5
+
+puts '####################################################################'
+# proc permite menos parâmetros que os definidos
+# proc não obriga a passar parâmetros mas pode causar erros. ex em multiplicações 5 x nil.
+l = proc do |parametro|
+  parametro = 0 if parametro.nil?
+  parametro * 5
+end
+
+puts l.call
+
+# definindo um método que recebe uma proc
+# & significa que o método está recebendo uma proc
+def metodo(&bloco)
+  bloco
+end
+
+l = metodo do
+  puts 'Cassio'
+end
+
+l.call
+
+l = metodo do |parametro|
+  puts parametro * 10
+end
+
+# proc aceita mais parãmetros que o definido mas so utiliza o necessário.
+l.call 5, 5, 5, 1000
