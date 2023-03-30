71c4ae32beabb146bac19ceb7ef6f826c635e823
Cassio Santana <cassioo.roger@gmail.com>
2023-03-30 06:31:35 -0300

    feat(aula-18-metaprogramming-modulo): definido métodos em escala

--- aula18_metaprogramming_modulo/aula18.rb
+++ aula18_metaprogramming_modulo/aula18.rb
@@ -1,18 +1,40 @@
 # metaprogramming - módulo
 
-cassio = 'teste'
-def cassio.olha
-  'teste cassio'
-end
+# cassio = 'teste'
+# def cassio.olha
+#   'teste cassio'
+# end
 
-# herança por comportamento
-cassio.olha
-a = cassio.clone
-puts a.olha
+# # herança por comportamento
+# cassio.olha
+# a = cassio.clone
+# puts a.olha
 
-b = cassio.dup
-puts b
-puts b.olha # undefined method `olha' for "teste":String
+# b = cassio.dup
+# puts b
+# puts b.olha # undefined method `olha' for "teste":String
 
 # clone = copia valores de variáveis e métodos (comportamentos)
 # dup = duplica sem as valores das variáveis mas não a metaprogramação
+
+###########################################################################
+cassio = 'teste'
+
+# forma de definir vários métodos ou atributos em escala de forma mais prática
+class << cassio
+  def olha1
+    'olha 1'
+  end
+
+  def olha2
+    'olha 2'
+  end
+
+  def olha3
+    'olha 3'
+  end
+end
+
+puts cassio.olha1
+puts cassio.olha2
+puts cassio.olha3
