d689c262779285c805d7e2cf98d5b3bd2f7eae10
Cassio Santana <cassioo.roger@gmail.com>
2023-03-31 12:53:34 -0300

    feat(aula-20-metaprogramming-eval-para-classes-e-instancias): obtendo valores privados com eval

--- aula20_metaprogramming_eval_para_classes_e_instancias/aula20.rb
+++ aula20_metaprogramming_eval_para_classes_e_instancias/aula20.rb
@@ -10,3 +10,22 @@ a = "puts 1 + 2"
 eval(a)
 puts a
 
+puts '----------------------------------------------------'
+class Teste
+  def initialize
+    @nome = 'Roger'
+  end
+
+  private
+
+  def metodo_privado
+    @nome
+  end
+end
+
+# Obtendo valores de atributos privados com eval
+# lembrando que variáveis de classe não são expostas
+# fora da classe.
+teste = Teste.new
+teste.instance_eval{puts @nome}
+teste.instance_eval{puts metodo_privado}
