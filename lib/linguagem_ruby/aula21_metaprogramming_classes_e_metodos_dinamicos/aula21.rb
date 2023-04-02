932d27ae251128ff500771946ed8c89486671a3e
Cassio Santana <cassioo.roger@gmail.com>
2023-04-02 16:34:42 -0300

    feat(aula-21-metaprogramming-classes-e-metodos-dinâmicos): definindo vários métodos dinâmicamente

--- lib/linguagem_ruby/aula21_metaprogramming_classes_e_metodos_dinamicos/aula21.rb
+++ lib/linguagem_ruby/aula21_metaprogramming_classes_e_metodos_dinamicos/aula21.rb
@@ -1,11 +1,32 @@
 # frozen_string_literal: true
 
+# aula 21 metaprogramming_classes_e_metodos_dinamicos
 module Aula21
   # define métodos em runtime
   class ClasseMetodoRuntime
     def self.definir(nome_metodo)
       define_method(nome_metodo) do
         'método runtime'
+      end
+    end
+  end
+
+  # não entendi. pesquisar posteriormente
+  # class ClasseEmRuntime
+  #   def definir_classe(classe)
+  #     classe = classe.capitalize
+  #     eval("class #{classe} end")
+  #     classe = Object.const_get(classe)
+  #   end
+  # end
+
+  # define vários métodos dinâmicamente
+  class VariosMetodosRuntime
+    def self.definir_varios(*metodos)
+      metodos.each do |metodo|
+        define_method(metodo) do
+          'metodo em runtime'
+        end
       end
     end
   end
