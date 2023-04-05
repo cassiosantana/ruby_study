32f03e8594b1938c08cc556a2fa321da55ebfca2
Cassio Santana <cassioo.roger@gmail.com>
2023-04-05 07:30:23 -0300

    test(aula-19): Adicionado contexto
    Foi adicionado contexto para os testes
    para manter-los mais organizado
    
    Ref #49

--- spec/lib/linguagem_ruby/aula19_metaprogramming_dinamicos/aula19_spec.rb
+++ spec/lib/linguagem_ruby/aula19_metaprogramming_dinamicos/aula19_spec.rb
@@ -2,18 +2,20 @@ require 'spec_helper'
 require 'linguagem_ruby/aula19_metaprogramming_dinamicos/aula19'
 
 RSpec.describe Aula19 do
-  it 'ArgumentError - Lambda sem parâmetro' do
-    l = ->(p) { p * 5 }
-    expect { l.call }.to raise_error ArgumentError
-  end
+  context 'Lamda' do
+    it 'ArgumentError - Lambda sem parâmetro' do
+      l = ->(p) { p * 5 }
+      expect { l.call }.to raise_error ArgumentError
+    end
 
-  it 'ArgumentError - Lambda com menos parâmetros' do
-    l = ->(x, y) { x * y }
-    expect { l.call 5 }.to raise_error ArgumentError
-  end
+    it 'ArgumentError - Lambda com menos parâmetros' do
+      l = ->(x, y) { x * y }
+      expect { l.call 5 }.to raise_error ArgumentError
+    end
 
-  it 'ArgumentError - Lambda com mais parâmetros' do
-    l = ->(x) { x * x }
-    expect { l.call 5, 6 }.to raise_error ArgumentError
+    it 'ArgumentError - Lambda com mais parâmetros' do
+      l = ->(x) { x * x }
+      expect { l.call 5, 6 }.to raise_error ArgumentError
+    end
   end
 end
