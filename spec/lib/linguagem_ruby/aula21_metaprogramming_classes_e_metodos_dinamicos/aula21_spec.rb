d1efdee7398ded48812ecc6abedf7303bab98de6
Cassio Santana <cassioo.roger@gmail.com>
2023-04-08 11:04:15 -0300

    test(aula-21): Criada classe dinâmicamente
    Ref #47

--- spec/lib/linguagem_ruby/aula21_metaprogramming_classes_e_metodos_dinamicos/aula21_spec.rb
+++ spec/lib/linguagem_ruby/aula21_metaprogramming_classes_e_metodos_dinamicos/aula21_spec.rb
@@ -4,21 +4,12 @@ require 'spec_helper'
 require 'linguagem_ruby/aula21_metaprogramming_classes_e_metodos_dinamicos/aula21'
 
 RSpec.describe Aula21 do
-  it 'Método em runtime' do
-    Aula21::ClasseMetodoRuntime.definir 'teste'
-    resultado = Aula21::ClasseMetodoRuntime.new.teste
+  it 'Criada classe dinâmicamente' do
+    classe = +'cassio'
+    classe.capitalize!
+    eval("class #{classe} end")
+    classe = Object.const_get(classe)
 
-    expect(resultado).to eq('método runtime')
-  end
-
-  it 'Métodos em lote' do
-    Aula21::VariosMetodosRuntime.definir_varios('metodo1', 'metodo2', 'metodo3')
-    resultado1 = Aula21::VariosMetodosRuntime.new.metodo1
-    resultado2 = Aula21::VariosMetodosRuntime.new.metodo2
-    resultado3 = Aula21::VariosMetodosRuntime.new.metodo3
-
-    expect(resultado1).to eq('metodo em runtime')
-    expect(resultado2).to eq('metodo em runtime')
-    expect(resultado3).to eq('metodo em runtime')
+    expect(classe.class).to eq(Class)
   end
 end
