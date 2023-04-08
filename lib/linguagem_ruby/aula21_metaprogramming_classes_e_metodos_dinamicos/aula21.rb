458a711af7bfd05a6be1789801bee99dba5cae20
Cassio Santana <cassioo.roger@gmail.com>
2023-04-08 10:51:43 -0300

    feat(aula-21): Adicionado classe TesteUM
    A classe criada possui um método de classe
    que serve para definir métodos
    
    Ref #47

--- lib/linguagem_ruby/aula21_metaprogramming_classes_e_metodos_dinamicos/aula21.rb
+++ lib/linguagem_ruby/aula21_metaprogramming_classes_e_metodos_dinamicos/aula21.rb
@@ -2,31 +2,11 @@
 
 # aula 21 metaprogramming_classes_e_metodos_dinamicos
 module Aula21
-  # define métodos em runtime
-  class ClasseMetodoRuntime
+  # classe teste 1
+  class TesteUm
     def self.definir(nome_metodo)
       define_method(nome_metodo) do
-        'método runtime'
-      end
-    end
-  end
-
-  # não entendi. pesquisar posteriormente
-  # class ClasseEmRuntime
-  #   def definir_classe(classe)
-  #     classe = classe.capitalize
-  #     eval("class #{classe} end")
-  #     classe = Object.const_get(classe)
-  #   end
-  # end
-
-  # define vários métodos dinâmicamente
-  class VariosMetodosRuntime
-    def self.definir_varios(*metodos)
-      metodos.each do |metodo|
-        define_method(metodo) do
-          'metodo em runtime'
-        end
+        'metodo definido'
       end
     end
   end
