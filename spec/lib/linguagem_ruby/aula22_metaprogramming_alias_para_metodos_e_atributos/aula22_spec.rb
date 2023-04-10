b522a859db7b0e33b0549f2d6c7557155dd0a3c4
Cassio Santana <cassioo.roger@gmail.com>
2023-04-10 06:52:21 -0300

    test(aula-22): Sobrescrito método herdado
    Ref #42

---
+++ spec/lib/linguagem_ruby/aula22_metaprogramming_alias_para_metodos_e_atributos/aula22_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'linguagem_ruby/aula22_metaprogramming_alias_para_metodos_e_atributos/aula22'
+
+RSpec.describe Aula22 do
+  it 'Sobrescrito método herdado' do
+    resultado = Aula22::ClasseTeste2.new.ola
+    expect(resultado).to eq('olá sobrescrito')
+  end
+end
