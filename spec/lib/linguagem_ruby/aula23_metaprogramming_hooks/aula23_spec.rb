b4ebd3cb20f6ccd3c60eee4a75e12cca3c8d6f74
Cassio Santana <cassioo.roger@gmail.com>
2023-04-11 10:00:10 -0300

    test(aula-23): Testada classe que conhece subclasses
    Ref #43

--- spec/lib/linguagem_ruby/aula23_metaprogramming_hooks/aula23_spec.rb
+++ spec/lib/linguagem_ruby/aula23_metaprogramming_hooks/aula23_spec.rb
@@ -4,4 +4,10 @@ require 'spec_helper'
 require 'linguagem_ruby/aula23_metaprogramming_hooks/aula23'
 
 RSpec.describe Aula23 do
+  it 'Classe que conhece as subclasses ' do
+    resultado = Pai.lista_de_classes
+    nome1 = Pai.lista_de_classes[0].new.mostre_o_nome
+    expect(resultado.length).to eq(3)
+    expect(nome1).to eq('Cassio')
+  end
 end
