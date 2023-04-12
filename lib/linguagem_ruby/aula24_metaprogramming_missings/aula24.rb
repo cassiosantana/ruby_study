8c63304a4c1c1e85a0b51ec54d8a2d549a41a176
Cassio Santana <cassioo.roger@gmail.com>
2023-04-12 17:09:13 -0300

    build: Adiciona estrutura aula 24
    Ref #41

---
+++ lib/linguagem_ruby/aula24_metaprogramming_missings/aula24.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# módulo aula 24
+module Aula24
+end
+
---
+++ spec/lib/linguagem_ruby/aula24_metaprogramming_missings/aula24_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'linguagem_ruby/aula24_metaprogramming_missings/aula24'
+
+RSpec.describe Aula24 do
+  it '' do
+  end
+end
