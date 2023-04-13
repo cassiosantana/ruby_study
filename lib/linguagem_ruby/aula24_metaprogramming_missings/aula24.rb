0403f090ac9ec0f4ee80ba7b9cc52c1bff2fddb7
Cassio Santana <cassioo.roger@gmail.com>
2023-04-12 22:01:38 -0300

    feat: Adicionados métodos dinâmicamente
    Utilizando o const_missing foi possível
    a partir de um aquivo definir class e métodos
    dinâmicamente.
    
    Ref #41

--- lib/linguagem_ruby/aula24_metaprogramming_missings/aula24.rb
+++ lib/linguagem_ruby/aula24_metaprogramming_missings/aula24.rb
@@ -30,16 +30,54 @@ end
 
 # Roger
 
-# Não entendi este código.
+# # Não entendi este código.
+# class Module
+#   old_const_missing = instance_method(:const_missing)
+#   define_method(:const_missing) do |nome|
+#     existe = File.exist?("lib/linguagem_ruby/aula24_metaprogramming_missings/#{nome}.csv")
+#     unless existe
+#       puts "Classe #{nome} não encontrada"
+#       return
+#     end
+#     old_const_missing.bind(self).call(nome)
+#   end
+# end
+# Cassio
+
+# Aqui o método const_missing foi sobrescrito de forma que ele executa diversas
+# operações. Basicamente o código tenta ler um arquivo no diretório especificado e caso
+# ele exista será criada uma classe com métodos baseados no conteúdo da primeira linha
+# do arquivo lido desde que cada nome de método esteja separado por ';'.
 class Module
-  old_const_missing = instance_method(:const_missing)
-  define_method(:const_missing) do |nome|
+  def const_missing(nome)
     existe = File.exist?("lib/linguagem_ruby/aula24_metaprogramming_missings/#{nome}.csv")
     unless existe
       puts "Classe #{nome} não encontrada"
       return
     end
-    old_const_missing.bind(self).call(nome)
+    const_set(nome, Class.new)
+    klass = const_get(nome)
+    File.open("lib/linguagem_ruby/aula24_metaprogramming_missings/#{nome}.csv") do |arquivo|
+      arquivo.each do |linha|
+        campos = linha.split(";")
+
+        campos.each do |campo|
+          klass.class_eval do
+            define_method("#{campo}=") do |valor|
+              instance_variable_set "@#{campo}", valor
+            end
+
+            define_method("#{campo}") do
+              instance_variable_get "@#{campo}"
+            end
+          end
+        end
+      end
+    end
+    klass
   end
 end
-Cassio
+
+c = Cassio.new
+c.nome = 'Santana'
+puts c.methods - Class.methods
