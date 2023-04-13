72bae0d337568bc588af6cd52054f70f115e6299
Cassio Santana <cassioo.roger@gmail.com>
2023-04-12 21:50:55 -0300

    feat: Sobrescrito const_missing 1
    Foi realizado um exemplo simples de sobrescrição
    do método const_missing como primeiro exemplo
    
    Ref #41

--- lib/linguagem_ruby/aula24_metaprogramming_missings/aula24.rb
+++ lib/linguagem_ruby/aula24_metaprogramming_missings/aula24.rb
@@ -4,3 +4,15 @@
 module Aula24
 end
 
+# sobrescrevendo método da classe Module do Ruby
+class Module
+  # const_missing é um método que retorna um erro
+  # caso uma constante não exista.
+  # aqui estamos o sobrescrevendo para que tenha
+  # o comportamento que desejamos.
+  def const_missing(nome)
+    puts "Classe #{nome} não encontrada"
+  end
+end
+
+Cassio
