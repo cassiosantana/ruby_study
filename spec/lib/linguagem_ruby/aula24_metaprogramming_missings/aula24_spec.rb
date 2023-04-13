963e6050ac2d286e8b84e4a052a2946dba0055e5
Cassio Santana <cassioo.roger@gmail.com>
2023-04-13 10:10:14 -0300

    test(aula24): Adicionado teste da classeteste 2
    Ref #41

--- spec/lib/linguagem_ruby/aula24_metaprogramming_missings/aula24_spec.rb
+++ spec/lib/linguagem_ruby/aula24_metaprogramming_missings/aula24_spec.rb
@@ -11,4 +11,8 @@ RSpec.describe 'Aula24.1' do
   it 'Constante não existe' do
     expect(ClasseTeste1::TESTE).to eq('Constante TESTE não existe')
   end
+
+  it 'Método de classe não existe' do
+    expect(ClasseTeste2.metodo1).to eq('Método de classe metodo1 não existe')
+  end
 end
