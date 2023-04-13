5e163067161628f128dbdd52a4ed708cb9fde424
Cassio Santana <cassioo.roger@gmail.com>
2023-04-13 10:15:03 -0300

    feat(aula24): Adicionanda ClasseTeste4
    Adicionada classe com method_missing
    que faz leitura de arquivo e retorna se
    dentro dele possui o campo passado como
    parâmetro
    
    Ref #41

--- lib/linguagem_ruby/aula24_metaprogramming_missings/aula24.1.rb
+++ lib/linguagem_ruby/aula24_metaprogramming_missings/aula24.1.rb
@@ -20,3 +20,16 @@ class ClasseTeste3
     "Método de instância #{nome} não existe"
   end
 end
+
+# Classe para teste 4
+class ClasseTeste4
+  def method_missing(nome)
+    File.open('lib/linguagem_ruby/aula24_metaprogramming_missings/Cassio.csv') do |arquivo|
+      arquivo.each do |linha|
+        campos = linha.split(';')
+        return "Campo #{nome} não existe" unless campos.include?(nome.to_s)
+      end
+    end
+    'Campo existe'
+  end
+end
