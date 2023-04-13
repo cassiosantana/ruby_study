4ac5022903f6f79c60a3cd3d9caa5d9f1fb30045
Cassio Santana <cassioo.roger@gmail.com>
2023-04-13 10:06:05 -0300

    test(aula24): Adicionado teste classe teste1
    Ref #41

--- spec/lib/linguagem_ruby/aula24_metaprogramming_missings/aula24_spec.rb
+++ spec/lib/linguagem_ruby/aula24_metaprogramming_missings/aula24_spec.rb
@@ -2,6 +2,13 @@
 
 require 'spec_helper'
 require 'linguagem_ruby/aula24_metaprogramming_missings/aula24'
+require 'linguagem_ruby/aula24_metaprogramming_missings/aula24.1'
 
 RSpec.describe 'Aula24' do
+end
+
+RSpec.describe 'Aula24.1' do
+  it 'Constante não existe' do
+    expect(ClasseTeste1::TESTE).to eq('Constante TESTE não existe')
+  end
 end
