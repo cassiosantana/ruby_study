121e27af0ce6998eb90bce1257b9ccccda9c753b
Cassio Santana <cassioo.roger@gmail.com>
2023-04-12 21:55:54 -0300

    feat: Criada classe com const_missing
    Ref #41

--- lib/linguagem_ruby/aula24_metaprogramming_missings/aula24.rb
+++ lib/linguagem_ruby/aula24_metaprogramming_missings/aula24.rb
@@ -4,15 +4,28 @@
 module Aula24
 end
 
-# sobrescrevendo método da classe Module do Ruby
-class Module
+# # sobrescrevendo método da classe Module do Ruby
+# class Module
   # const_missing é um método que retorna um erro
   # caso uma constante não exista.
   # aqui estamos o sobrescrevendo para que tenha
   # o comportamento que desejamos.
+#   def const_missing(nome)
+#     puts "Classe #{nome} não encontrada"
+#   end
+# end
+
+# Cassio
+
+# Cria classe caso não exista
+class Module
   def const_missing(nome)
-    puts "Classe #{nome} não encontrada"
+    existe = File.exist?("lib/linguagem_ruby/aula24_metaprogramming_missings/#{nome}.csv")
+    unless existe
+      puts "Classe #{nome} não encontrada"
+    end
+    const_set(nome, Class.new) # retorna a classe
   end
 end
 
-Cassio
+Roger
