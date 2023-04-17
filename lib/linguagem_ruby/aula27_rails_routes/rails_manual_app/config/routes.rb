d742c9bb71e588889c6ee3be44d7e175936e1d83
Cássio Santana <cassioo.roger@gmail.com>
2023-04-17 11:14:09 -0300

    Revert "Feature/controller home"

--- lib/linguagem_ruby/aula27_rails_routes/rails_manual_app/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  # rota para a página inicial da aplicação
-  root 'home#index'
-end
