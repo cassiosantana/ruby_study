c0092c5380bbaf7db857fcf5ccfec8d442d659f7
Cassio Santana <cassioo.roger@gmail.com>
2023-04-19 06:34:23 -0300

    build(aula29): Clonado projeto aula anterior
    Foi copiado o projeto da aula anterior para
    dar continuidade visando manter a estrutura
    de uma pasta por aula
    
    Ref #36

---
+++ lib/linguagem_ruby/aula29_rails_migrations/rails_manual_app/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  # rota para a página inicial da aplicação
+  root 'home#index'
+
+  # rota para página sobre
+  get '/sobre', to: 'sobre#index'
+end
