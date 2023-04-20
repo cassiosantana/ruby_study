f68d456e4c25c0f388f3a0ea8cfc7dde368557d3
Cassio Santana <cassioo.roger@gmail.com>
2023-04-20 11:10:44 -0300

    buil(aula29): Adiconado coluna a tabela produto2
    Ref #36

--- lib/linguagem_ruby/aula29_rails_migrations/rails_manual_app/db/schema.rb
+++ lib/linguagem_ruby/aula29_rails_migrations/rails_manual_app/db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_20_130733) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_20_134632) do
   create_table "produto2s", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "nome"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "descricao"
   end
 
   create_table "produtos1", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "nome"
+    t.text "descricao"
   end
 
 end
