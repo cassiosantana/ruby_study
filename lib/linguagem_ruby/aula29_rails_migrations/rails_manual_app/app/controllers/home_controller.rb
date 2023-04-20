11ef448a2dec3121f8e920e11d7c94598f861eb4
Cassio Santana <cassioo.roger@gmail.com>
2023-04-20 09:50:18 -0300

    test(aula29): Atualizado produto
    Teste atualizando produto manualmente
    diretamente no controller.
    
    Ref #36

--- lib/linguagem_ruby/aula29_rails_migrations/rails_manual_app/app/controllers/home_controller.rb
+++ lib/linguagem_ruby/aula29_rails_migrations/rails_manual_app/app/controllers/home_controller.rb
@@ -3,9 +3,15 @@ class HomeController < ApplicationController
     @teste = 'Cássio'
 
     # inserir produto
+    # produto = Produto.new
+    # produto.nome = 'Pneu traseiro'
+    # produto.salvar
+
+    # atualizar
     produto = Produto.new
-    produto.nome = 'Pneu traseiro'
-    produto.salvar
+    produto.id = 4
+    produto.nome = 'Pneu dianteiro'
+    produto.atualizar
 
     @produtos = Produto.buscar
   end
