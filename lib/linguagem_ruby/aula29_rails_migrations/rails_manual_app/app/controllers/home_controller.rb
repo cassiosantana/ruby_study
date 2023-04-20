f6eb71208536882778376ce39f18c3eba299ab27
Cassio Santana <cassioo.roger@gmail.com>
2023-04-20 09:46:02 -0300

    test(aula29): Adicionado produto
    Teste adicionando produto manualmente
    diretamente no controller.
    
    Ref #36

--- lib/linguagem_ruby/aula29_rails_migrations/rails_manual_app/app/controllers/home_controller.rb
+++ lib/linguagem_ruby/aula29_rails_migrations/rails_manual_app/app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
   def index
     @teste = 'Cássio'
+
+    # inserir produto
+    produto = Produto.new
+    produto.nome = 'Pneu traseiro'
+    produto.salvar
+
     @produtos = Produto.buscar
   end
 end
