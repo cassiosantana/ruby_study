5dc598dbe0820cac3f66b8660d26ee107f88e5d8
Cassio Santana <cassioo.roger@gmail.com>
2023-04-20 09:42:27 -0300

    build(aula29): Adicionado método salvar
    Ref #36

--- lib/linguagem_ruby/aula29_rails_migrations/rails_manual_app/app/models/produto.rb
+++ lib/linguagem_ruby/aula29_rails_migrations/rails_manual_app/app/models/produto.rb
@@ -1,10 +1,14 @@
 class Produto
   attr_accessor :id, :nome
-  def initialize(attrs)
+  def initialize(attrs = nil)
     if attrs.present?
       self.id = attrs['id']
       self.nome = attrs['nome']
     end
+  end
+
+  def salvar
+    ActiveRecord::Base.connection.execute("INSERT INTO produtos1 (nome) VALUES ('#{self.nome}');")
   end
 
   def self.buscar
