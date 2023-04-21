e7cc60717a8b7bb115a5c8d3075242478cb40bae
Cassio Santana <cassioo.roger@gmail.com>
2023-04-21 16:00:48 -0300

    test(aula14): Adicionado FFaker ao teste título
    Teste Refatorado teste utilizando FFaker
    
    Closes #15

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe User, type: :model do
     nickname = FFaker::Name.first_name
     kind = %i[knight wizard].sample
     level = FFaker::Random.rand(1..99)
-    user = User.create(nickname: nickname, kind: kind, level: level)
+    user = create(:user, nickname: nickname, kind: kind, level: level)
     expect(user.title).to eq("#{kind} #{nickname} ##{level}")
   end
 end
