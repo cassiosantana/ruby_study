7dd70b843d3fb182482be0290e42b2c1a4c7d0ae
Cassio Santana <cassioo.roger@gmail.com>
2023-04-21 15:04:17 -0300

    test(aula13): Testado nome correto do herói
    Refatorado o teste se o nome do herói está correto
    
    Closes # 14

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,10 @@ RSpec.describe User, type: :model do
     expect(user).to_not be_valid
   end
   it 'returns the correct hero title' do
-    user = User.create(nickname: 'Chronos', kind: :wizard, level: 1)
-    expect(user.title).to eq('wizard Chronos #1')
+    nickname = FFaker::Name.first_name
+    kind = %i[knight wizard].sample
+    level = FFaker::Random.rand(1..99)
+    user = User.create(nickname: nickname, kind: kind, level: level)
+    expect(user.title).to eq("#{kind} #{nickname} ##{level}")
   end
 end
