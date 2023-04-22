0f0677a7e547e7f9e99a897697e69d68e51de05d
Cassio Santana <cassioo.roger@gmail.com>
2023-04-22 07:19:59 -0300

    build(aula30): Adicionado action incluir
    Ref #35

--- lib/linguagem_ruby/aula30_rails_forms/rails_manual_app/app/controllers/produtos_controller.rb
+++ lib/linguagem_ruby/aula30_rails_forms/rails_manual_app/app/controllers/produtos_controller.rb
@@ -1,5 +1,15 @@
 class ProdutosController < ApplicationController
+  before_action :carregar_produtos, except: [:incluir, :salvar]
+
   def index
-    @produtos = Produto2.all
+  end
+
+  def incluir
+    produto = Produto2.new
+    produto.nome = params[:nome]
+    produto.descricao = params[:descricao]
+    produto.save
+
+    redirect_to '/produtos'
   end
 end
