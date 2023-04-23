25096b17aedb6e2b1fb22185e64330b54f1edf6f
Cassio Santana <cassioo.roger@gmail.com>
2023-04-23 06:00:22 -0300

    build(aula31): Copiado Projeto aula 30
    Foi criado o projeto da aula anterior para
    que a organização continue sendo uma pasta
    por aula.
    
    Ref #34

---
+++ lib/linguagem_ruby/aula31_rails_initializers/rails_manual_app/app/controllers/produtos_controller.rb
@@ -0,0 +1,42 @@
+class ProdutosController < ApplicationController
+  before_action :carregar_produtos, except: [:incluir, :salvar]
+
+  def index
+  end
+
+  def incluir
+    produto = Produto2.new
+    produto.nome = params[:nome]
+    produto.descricao = params[:descricao]
+    produto.save
+
+    redirect_to '/produtos'
+  end
+
+  def alterar
+    @produto = Produto2.find(params[:id])
+    render :index
+  end
+
+  def salvar
+    produto = Produto2.find(params[:id])
+    produto.nome = params[:nome]
+    produto.descricao = params[:descricao]
+    produto.save
+
+    redirect_to '/produtos'
+  end
+
+  def excluir
+    produto = Produto2.find(params[:id])
+    produto.destroy
+
+    redirect_to '/produtos'
+  end
+
+  private
+
+  def carregar_produtos
+    @produtos = Produto2.all
+  end
+end
