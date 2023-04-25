59bd6f543de77dfa3fe293b62a60ddeed95ba741
Cassio Santana <cassioo.roger@gmail.com>
2023-04-25 16:10:05 -0300

    test(aula15): Adicionado Level below allowed
    Ref #16

--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 RSpec.describe Weapon, type: :model do
   describe 'Weapon level' do
     let(:level_above) { build(:weapon, level: FFaker::Random.rand(100..1000)) }
+    let(:level_below) { build(:weapon, level: FFaker::Random.rand(-1000..0)) }
 
     context 'Invalid level' do
       it 'Level above allowed' do
         expect(level_above).to_not be_valid
+      end
+
+      it 'Level below the allowed' do
+        expect(level_below).to_not be_valid
       end
     end
   end
