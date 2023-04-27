02cd0f84e691db864709e429f0d5a07eaedaa6c6
Cassio Santana <cassioo.roger@gmail.com>
2023-04-27 11:09:22 -0300

    build(aula34): Adicionado seed
    Ref #77

--- lib/linguagem_ruby/aula34_rails_rake/rails_manual_app/db/seeds.rb
+++ lib/linguagem_ruby/aula34_rails_rake/rails_manual_app/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Produto2.new(nome: 'Cadeira Gamer', descricao: 'Cadeira muito conformável e linda').save
