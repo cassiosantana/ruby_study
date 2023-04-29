0468a3812c51d56149631ce201bbb5159aefebab
Cassio Santana <cassioo.roger@gmail.com>
2023-04-29 15:31:08 -0300

    test(aula19): Adicionado POST /create
    Ref #23

--- spec/requests/weapons_spec.rb
+++ spec/requests/weapons_spec.rb
@@ -38,4 +38,13 @@ RSpec.describe "Weapons", type: :request do
       end
     end
   end
+
+  describe "POST /create" do
+    it '' do
+      weapon_attributes = FactoryBot.attributes_for(:weapon)
+      post weapons_path, params: { weapon: weapon_attributes }
+      # checks if the last user sent in the post has the same attributes that were created before the post
+      expect(Weapon.last).to have_attributes(weapon_attributes)
+    end
+  end
 end
