65d56c2043d5ac446419439df6f441f0af83eccb
Cassio Santana <cassioo.roger@gmail.com>
2023-04-29 15:30:18 -0300

    refactor(aula19): Removido testes inúteis
    Ref #23

--- spec/requests/weapons_spec.rb
+++ spec/requests/weapons_spec.rb
@@ -38,25 +38,4 @@ RSpec.describe "Weapons", type: :request do
       end
     end
   end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/weapons/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/weapons/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/weapons/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
 end
