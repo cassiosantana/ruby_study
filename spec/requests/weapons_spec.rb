38f8ec7167ee809b3e9adf49b842b34cb5d93e81
Cassio Santana <cassioo.roger@gmail.com>
2023-04-29 13:02:29 -0300

    refactor(aula19): Organizado testes por contexto
    Ref #23

--- spec/requests/weapons_spec.rb
+++ spec/requests/weapons_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Weapons", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/weapons/index"
-      expect(response).to have_http_status(:success)
+    context 'http status' do
+      it "returns http success" do
+        get "/weapons/index"
+        expect(response).to have_http_status(:success)
+      end
     end
-  end
 
-  describe "Weapons attributes" do
-    context 'Present attributes' do
+    context 'present attributes' do
       it "the weapon name is present" do
         weapons = create_list(:weapon, 3)
         get weapons_path
@@ -56,5 +56,4 @@ RSpec.describe "Weapons", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
-
 end
