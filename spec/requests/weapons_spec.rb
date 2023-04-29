aebf5e6d642a2596444f77e3b7a905ae82f9a770
Cassio Santana <cassioo.roger@gmail.com>
2023-04-29 15:25:59 -0300

    refactor(aula19): Removido teste desnecessário
    Ref #23

--- spec/requests/weapons_spec.rb
+++ spec/requests/weapons_spec.rb
@@ -2,13 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Weapons", type: :request do
   describe "GET /index" do
-    context 'http status' do
-      it "returns http success" do
-        get "/weapons/index"
-        expect(response).to have_http_status(:success)
-      end
-    end
-
     context 'present attributes' do
       it "the weapon name is present" do
         weapons = create_list(:weapon, 3)
