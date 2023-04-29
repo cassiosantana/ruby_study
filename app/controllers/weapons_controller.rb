6910af20d72b7a9129f1f2b61c78e4489d44433c
Cassio Santana <cassioo.roger@gmail.com>
2023-04-29 15:29:05 -0300

    build(aula19): Adicionados create e weapon params
    Ref #23

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -4,11 +4,19 @@ class WeaponsController < ApplicationController
   end
 
   def create
+    @weapon = Weapon.create(weapon_params)
+    redirect_to weapons_path
   end
 
   def destroy
   end
 
   def show
+  end
+
+  private
+
+  def weapon_params
+    params.require(:weapon).permit(:name, :description, :power_base, :power_step, :level)
   end
 end
