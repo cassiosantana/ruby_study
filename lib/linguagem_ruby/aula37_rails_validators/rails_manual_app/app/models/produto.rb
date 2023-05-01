688288c0ae69601cc6d6f0f99970acee64699420
Cassio Santana <cassioo.roger@gmail.com>
2023-05-01 06:00:42 -0300

    build(aula37): Adicionado projeto
    Copiado projeto da aula anterior para manter a estrutura de uma pasta por aula
    
    Ref #94

---
+++ lib/linguagem_ruby/aula37_rails_validators/rails_manual_app/app/models/produto.rb
@@ -0,0 +1,26 @@
+class Produto
+  attr_accessor :id, :nome
+  def initialize(attrs = nil)
+    if attrs.present?
+      self.id = attrs['id']
+      self.nome = attrs['nome']
+    end
+  end
+
+  def salvar
+    ActiveRecord::Base.connection.execute("INSERT INTO produtos1 (nome) VALUES ('#{self.nome}');")
+  end
+
+  def atualizar
+    ActiveRecord::Base.connection.execute("UPDATE produtos1 SET nome= '#{self.nome}' WHERE id=#{self.id};")
+  end
+
+  def excluir
+    ActiveRecord::Base.connection.execute("DELETE FROM produtos1 WHERE id=#{self.id};")
+  end
+
+  def self.buscar
+    produtos = ActiveRecord::Base.connection.exec_query('select * from produtos1')
+    produtos.map { |p| Produto.new(p) }
+  end
+end
---
+++ lib/linguagem_ruby/aula37_rails_validators/rails_manual_app/app/views/produtos/index.json.jbuilder
@@ -0,0 +1,4 @@
+json.array! @produtos do |produto|
+  json.id produto.id
+  json.nome produto.nome
+end
