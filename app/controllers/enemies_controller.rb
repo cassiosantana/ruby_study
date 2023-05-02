f02e8366f0c506a42a06d028543d3de9c34fdd21
Cassio Santana <cassioo.roger@gmail.com>
2023-05-02 17:03:40 -0300

    build(aula21): Adiciona método set_enemies
    Ref #25

--- app/controllers/enemies_controller.rb
+++ app/controllers/enemies_controller.rb
@@ -1,7 +1,16 @@
 class EnemiesController < ApplicationController
+  before_action :set_enemies
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def set_enemies
+    @enemy = Enemy.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { message: e.message }, status: :not_found
   end
 end
