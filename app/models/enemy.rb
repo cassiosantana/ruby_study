c53f13abd226fd8d0dab88a1eab47aaf614ebe3f
Cassio Santana <cassioo.roger@gmail.com>
2023-05-02 17:01:29 -0300

    build(aula21): Adiciona validações ao Enemy
    Ref #25

--- app/models/enemy.rb
+++ app/models/enemy.rb
@@ -1,2 +1,5 @@
 class Enemy < ApplicationRecord
+  enum kind: [ :goblin, :orc, :demon, :dragon ]
+  validates :level, numericality: { greater_than: 0, less_than_or_equal_to: 99 }
+  validates_presence_of :name, :power_base, :power_step, :level, :kind
 end
