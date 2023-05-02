3266474cbec43f77cd06ac80df926be8b260c12e
Cassio Santana <cassioo.roger@gmail.com>
2023-05-02 15:09:03 -0300

    build(aula03): Adiciona path arquivo aula 03

--- lib/linguagem_ruby.rb
+++ lib/linguagem_ruby.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "linguagem_ruby/version"
+require_relative 'linguagem_ruby/aula03_variaveis_e_data_types/aula03'
 
 module LinguagemRuby
   class Error < StandardError; end
