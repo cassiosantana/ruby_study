8bd8f03b789b861aa5161b7663b8be8a756d7602
Cassio Santana <cassioo.roger@gmail.com>
2023-05-05 07:18:06 -0300

    build(aula39): Adiciona migrate
    Ref #96

--- lib/linguagem_ruby/aula39_rails_estruturas_dependentes/my_app/db/schema.rb
+++ lib/linguagem_ruby/aula39_rails_estruturas_dependentes/my_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_124808) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_05_100741) do
   create_table "carros", force: :cascade do |t|
     t.string "nome"
     t.integer "marca_id", null: false
@@ -20,11 +20,31 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_124808) do
     t.index ["marca_id"], name: "index_carros_on_marca_id"
   end
 
+  create_table "item_pedidos", force: :cascade do |t|
+    t.integer "pedido_id", null: false
+    t.integer "carro_id", null: false
+    t.float "valor"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["carro_id"], name: "index_item_pedidos_on_carro_id"
+    t.index ["pedido_id"], name: "index_item_pedidos_on_pedido_id"
+  end
+
   create_table "marcas", force: :cascade do |t|
     t.string "nome"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "pedidos", force: :cascade do |t|
+    t.integer "client_id"
+    t.float "valor_total"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "carros", "marcas"
+  add_foreign_key "item_pedidos", "carros"
+  add_foreign_key "item_pedidos", "pedidos"
 end
