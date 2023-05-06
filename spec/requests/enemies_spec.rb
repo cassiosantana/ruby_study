643d23b29d777bd504382ec98b39af26bbb14ea6
Cassio Santana <cassioo.roger@gmail.com>
2023-05-06 12:37:33 -0300

    test(aula22): Adiciona teste
    retorna status 200
    
    Ref #26

--- spec/requests/enemies_spec.rb
+++ spec/requests/enemies_spec.rb
@@ -1,18 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Enemies", type: :request do
-  describe "GET /update" do
-    it "returns http success" do
-      get "/enemies/update"
-      expect(response).to have_http_status(:success)
+  describe "PUT /enemies" do
+    context 'when the enemy exists' do
+      it 'return status code 200' do
+        enemy = create(:enemy)
+        enemy_attributes = attributes_for(:enemy)
+        put "/enemies/#{enemy.id}", params: enemy_attributes
+        expect(response).to have_http_status(200)
+      end
+
+      it 'updates the record'
+      it 'returns the enemy updated'
+    end
+
+    context 'when the enemy does not exist' do
+      it 'returns status code 404'
+      it 'returns a not found message'
     end
   end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/enemies/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
