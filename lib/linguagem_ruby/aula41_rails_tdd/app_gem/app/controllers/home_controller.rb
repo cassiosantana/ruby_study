43fc09d5ee37c732a1f51d0e250c58149584e2d5
Cassio Santana <cassioo.roger@gmail.com>
2023-05-10 06:46:42 -0300

    build(aula41): Adiciona valores às propriedades do model cliente
    Ref #98

--- lib/linguagem_ruby/aula41_rails_tdd/app_gem/app/controllers/home_controller.rb
+++ lib/linguagem_ruby/aula41_rails_tdd/app_gem/app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
     @cliente = Cliente.new
+    @cliente.nome = "Cassio"
+    @cliente.telefone = "81 9977-9977"
+    @cliente.cpf = "77777777777"
+
     @valor = FormatString::Moeda.br(params[:moeda])  if params[:moeda].present?
   end
 end
