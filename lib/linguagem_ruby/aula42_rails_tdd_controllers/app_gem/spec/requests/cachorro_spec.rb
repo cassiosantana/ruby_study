5c352712661bff8de8b41edcdacbd27a0996b43e
Cassio Santana <cassioo.roger@gmail.com>
2023-05-11 08:42:50 -0300

    fix(aula42): Corrige o describe do teste
    Ref #107

--- lib/linguagem_ruby/aula42_rails_tdd_controllers/app_gem/spec/requests/cachorro_spec.rb
+++ lib/linguagem_ruby/aula42_rails_tdd_controllers/app_gem/spec/requests/cachorro_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe CachorroController, type: :request do
+RSpec.describe CachorrosController, type: :request do
   describe "GET /index" do
     it 'ter a variavel de classe @cachorros' do
       Cachorro.destroy_all
