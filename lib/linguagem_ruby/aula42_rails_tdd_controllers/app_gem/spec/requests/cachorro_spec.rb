c376960e0b452a8875202ee38d73afc0f0fc4587
Cassio Santana <cassioo.roger@gmail.com>
2023-05-12 11:56:42 -0300

    test(aula42): Adiciona test response.body
    Closes #107

--- lib/linguagem_ruby/aula42_rails_tdd_controllers/app_gem/spec/requests/cachorro_spec.rb
+++ lib/linguagem_ruby/aula42_rails_tdd_controllers/app_gem/spec/requests/cachorro_spec.rb
@@ -18,5 +18,15 @@ RSpec.describe CachorrosController, type: :request do
       get cachorros_path
       expect(response.status).to eq(200)
     end
+
+    it 'verifica se o código foi inserido na página' do
+      Cachorro.create(nome: 'Boomer', raca: 'Beagle')
+      Cachorro.create(nome: 'Austin', raca: 'Beagle')
+      Cachorro.create(nome: 'Feeder', raca: 'Beagle')
+
+      get '/cachorros'
+      puts response.body
+      expect(response.body).to match /<ul>.*?<li>/im
+    end
   end
 end
